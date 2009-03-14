eba9d491c062c7f68715ca1279600136282c18c2
Jim V <landham@gmail.com>
2009-03-14 12:17:13 -0500

    added drop down menu for groups. Tested only in firefox. Utilizing jquery framework. See public/javascript/site.js for how it works.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,3 +1,26 @@
-jQuery(document).ready(function(){
-	
+var timeout    = 500;
+var closetimer = 0;
+var ddmenuitem = 0;
+
+function jsddm_open()
+{  jsddm_canceltimer();
+   jsddm_close();
+   ddmenuitem = jQuery(this).find('ul').css('visibility', 'visible');}
+
+function jsddm_close()
+{  if(ddmenuitem) ddmenuitem.css('visibility', 'hidden');}
+
+function jsddm_timer()
+{  closetimer = window.setTimeout(jsddm_close, timeout);}
+
+function jsddm_canceltimer()
+{  if(closetimer)
+   {  window.clearTimeout(closetimer);
+      closetimer = null;}}
+
+jQuery(document).ready(function()
+{  jQuery('#dropdown > li').bind('mouseover', jsddm_open)
+   jQuery('#dropdown > li').bind('mouseout',  jsddm_timer)
 });
+
+document.onclick = jsddm_close;
