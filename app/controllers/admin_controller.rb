6f9753dd79291dd075e2b7b613c904aa99530eb4
Jim V <landham@gmail.com>
2009-03-14 10:28:23 -0500

    added note about groups

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
   def index
+    # NOTE: @groups is already a variable 
+    #  populated in the app/controllers/application.rb
+    #  file , so you don't have to run that find again.
   end
 end
