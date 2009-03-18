2409bf31d985583c6ee9c8b8d7741d951ff5dce1
vlandham <landham@gmail.com>
2009-03-18 11:37:29 -0500

    added some helper methods to window to combine packet values

--- app/models/window.rb
+++ app/models/window.rb
@@ -6,8 +6,21 @@ class Window < ActiveRecord::Base
     self.start_time <= time and self.end_time >= time
   end
   
+  # direction can be :incoming, :outgoing, or :all
+  # metric is by default just bytes, but it can be :kilobytes
+  #  or some larger metric
   def data(direction, metric = :bytes)
     value = self.send(("size_packets_"+direction.to_s).to_sym)
-    value.to_f / 1.send(metric).to_f
+    value.to_f / 1.send(metric.to_sym).to_f
+  end
+  
+  # combines both incoming and outgoing packet sizes
+  def size_packets_all
+    self.size_packets_incoming + self.size_packets_outgoing
+  end
+  
+  # combines both incoming and outgoing packet numbers
+  def num_packets_all
+    self.num_packets_incoming + self.num_packets_outgoing
   end
 end
