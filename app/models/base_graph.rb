5c450cf9ff340ad752bbd30da4e130b47a3bd592
Jim V <landham@gmail.com>
2009-04-04 17:49:00 -0500

    added base graph model

---
+++ app/models/base_graph.rb
@@ -0,0 +1,35 @@
+class BaseGraph
+  # title should be unique - for the top part of the sliders
+  # width and height used for display
+  attr_accessor :title, :width, :height
+  
+  # options could have
+  # :width & :height
+  def initialize(title, options = {})
+    @title = title
+    @width = options[:width] || 500
+    @height = options[:height] || 
+  end
+  
+  # bring in the raw data and process it as needed
+  def preprocess(raw_data)
+    puts "Should overwrite preprocess method"
+  end
+  
+  
+  # we have access to the builder class,
+  # so we can create and use the builder here.
+  # perhaps saving the resulting graph in @ofc
+  def postprocess()
+  end
+  
+  # Get the raw ofc graph out of our graph shell
+  def ofc_graph
+    @ofc
+  end
+  
+  # converts the title into a unique name
+  def name
+    self.title.lowercase.split(' ').join("_")
+  end
+end
