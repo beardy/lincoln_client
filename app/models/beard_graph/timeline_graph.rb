17f59888d3e83c5995dd648592b5e1cbe5f1a969
vlandham <landham@gmail.com>
2009-04-13 10:34:00 -0500

    everything is ready for the timeline graphs

---
+++ app/models/beard_graph/timeline_graph.rb
@@ -0,0 +1,17 @@
+module BeardGraph
+  class TimelineGraph < BaseGraph
+    
+    def preprocess
+    end
+  
+    def process
+    end
+  
+    def postprocess
+    end
+  
+    def generate_graph  
+    end
+    
+  end
+end
